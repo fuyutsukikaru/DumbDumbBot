1cbd04b2763da43e13bd755fac7061c1922af191
Soomin Jeong <hirokinoryu@gmail.com>
2014-08-04 18:06:26 -0700

    Bugs fixed

--- dumbdumb_modules/module.js
+++ dumbdumb_modules/module.js
@@ -11,7 +11,7 @@ module.exports = function(bot) {
   });
   // Listen for any message, say to him/her in the room
   bot.addListener("message", function(from, to, text, message) {
-    var arr = text.split();
+    var arr = text.split(" ", 2);
     var command = arr[0];
     var rest = text.substr(text.indexOf(' ') + 1);
     if (command == "!hi") {
