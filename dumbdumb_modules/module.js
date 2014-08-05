919742b90d9b92cc37efa964f70d49c9bbe4afda
Soomin Jeong <hirokinoryu@gmail.com>
2014-08-05 01:13:01 -0700

    Added more commands

--- dumbdumb_modules/module.js
+++ dumbdumb_modules/module.js
@@ -1,6 +1,10 @@
 var twitter = require('../twitter.js');
+var sakis = require('../sakis.js');
 
 module.exports = function(bot) {
+  bot.addListener("ping", function(server) {
+    // Wake up from idle
+  });
   bot.addListener("join", function(channel, nick, message) {
     if (nick == "Meball") {
       bot.say(channel, "HI " + nick.toUpperCase());
@@ -75,12 +79,41 @@ module.exports = function(bot) {
         bot.say(to, "I'm not a lolicon, I just like petite girls!");
       }
     }
+    else if (command == "!remind") {
+      var time = arr[1];
+      if (time == parseInt(time)) {
+        var reminder = text.substr(text.indexOf(' ') + 2);
+        bot.say(to, "Reminder set for " + from);
+        setTimeout(function() { bot.say(to, from + ": " + reminder); }, 60000 * time);
+      } else {
+        bot.say(to, "Need to set a valid number for minutes!");
+      }
+    }
+    else if (command == "!saki") {
+      var characters = sakis.characters;
+      var len = from.length;
+      var d = new Date();
+      var date = d.getDate();
+      var day = d.getDay();
+      var val = len * date * day;
+      var hash = val % 176;
+      var saki = characters[hash];
+      bot.say(to, "Your spirit Saki today is " + saki);
+      var chars = saki.split(" ", 2);
+      if (!chars[1]) {
+        bot.say(to, "http://saki.wikia.com/wiki/" + chars[0]);
+      } else {
+        bot.say(to, "http://saki.wikia.com/wiki/" + chars[0] + "_" + chars[1]);
+      }
+    }
     else if (command == "!help") {
       bot.say(to, "Enter !hi or !sup to have DumbDumbBot greet you!");
       bot.say(to, "Enter !goaway <name of person> to have DumbDumbBot be rude to a person.");
       bot.say(to, "Enter !countdown <number> to have DumbDumbBot start a countdown! Limited to numbers under 15.");
+      bot.say(to, "Enter !reminder <minutes> <message> to have DumbDumbBot remind you of things.");
       bot.say(to, "Enter !tweet <message to send> to post a tweet.");
       bot.say(to, "Enter !yakuman to have DumbDumbBot tell you what yakuman you should go for today.");
+      bot.say(to, "Enter !saki to get your spiritual Saki of the day.");
     }
   });
 };
