7a3f940ecb178a7676e0600ec54761cf4a4a9c23
Soomin Jeong <hirokinoryu@gmail.com>
2014-08-25 19:45:14 -0700

    Fixed express issues

--- run.js
+++ run.js
@@ -1,8 +1,20 @@
 var irc = require("irc"), fs = require("fs");
 var pjson = require("./package.json");
 var config = require("./config");
+var express = require("express");
+var app = express();
+
+// Setup middleware
+app.use(express.cookieParser('tweet much?'));
+app.use(express.session());
+app.use(express.static(__dirname));
 
 var bot = new irc.Client(config.server, config.nick, config);
 fs.readdirSync("./dumbdumb_modules").forEach(function(file) {
 	require("./dumbdumb_modules/" + file)(bot);
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
