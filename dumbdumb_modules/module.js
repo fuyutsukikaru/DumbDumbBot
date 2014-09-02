fa55acde72de8a091ae77c72845675fc4ddf9c92
Soomin Jeong <hirokinoryu@gmail.com>
2014-09-01 18:20:31 -0700

    Changed API key

--- dumbdumb_modules/module.js
+++ dumbdumb_modules/module.js
@@ -130,7 +130,7 @@ module.exports = function(bot) {
     }
     if (youtubeurl.test(text) && command != "!hide") {
       var urls = youtubeurl.exec(text);
-      youtube.setKey('AIzaSyCCR3BpyfjRun-ni-u41CmW6yq074P1AaU');
+      youtube.setKey('AIzaSyB1OOSpTREs85WUMvIgJvLTZKye4BVsoFU');
       console.log("Match youtube link!");
       var id = youtubeid(urls[0]);
       console.log(id);
