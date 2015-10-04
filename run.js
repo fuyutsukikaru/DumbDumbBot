b4c9fe3eb0a2e11a781453930af973fe4554b532
Soomin Jeong <sjeongus@gmail.com>
2015-10-04 02:27:11 -0700

    Loading static html

--- run.js
+++ run.js
@@ -2,10 +2,19 @@ var http = require("http");
 var irc = require("irc");
 var pjson = require("./package.json");
 var config = require("./dumbdumb_modules/config");
+var fs = require('fs');
 
 var bot = new irc.Client(config.server, config.nick, config);
 require("./dumbdumb_modules/module.js")(bot);
 
-http.createServer(function(request, response) {
-  console.log("Connected");
-}).listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 10024, process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+fs.readFile('./index.html', function (err, html) {
+  if (err) {
+    throw err;
+  }
+  http.createServer(function(request, response) {
+    console.log("Connected");
+    response.writeHeader(200, {"Content-Type": "text/html"});
+    response.write(html);
+    response.end();
+  }).listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 10024, process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+}
