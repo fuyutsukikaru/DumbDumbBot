1ffcd8ce48d5c5a83b024d1209bf6f0317cc4f5a
Soomin Jeong <sjeongus@gmail.com>
2018-08-10 12:59:08 -0700

    Update twit.js

--- dumbdumb_modules/twitter/twit.js
+++ dumbdumb_modules/twitter/twit.js
@@ -2,17 +2,17 @@ var twit = require('twit');
 var irc = require('irc');
 
 var twitter = new twit({
-  consumer_key: 'tpNGGaBKwUA3Mys2XJr6g6rzO',
-  consumer_secret: 'aZdycgA2oYkGOEIWcve9ADypgN4sEIICsOufHw9cws5qLXFbnC',
-  access_token: '1687430976-yxuGeSQzd9xVh5RijFn0Yi85aLHG67XNwJV9mum',
-  access_token_secret: 'D6dlbNsFmanFlpw7oZmYanO9zuZky5nn4aFBiaCj4xC4u'
+  consumer_key: '',
+  consumer_secret: '',
+  access_token: '',
+  access_token_secret: ''
 });
 
 var twitter2 = new twit({
-  consumer_key: 'EfnyxsZDM4sRsPqz7NEe4Nl3l',
-  consumer_secret: 'ybIVkwktyOFMZTgRokRSmEHs2cw1sChbwFJvJWRIYJ0TZ7FEdQ',
-  access_token: '2777981995-z4dP9RhyHZ5ibDEhZ9zKJHN3DnWCel7ZU83iVAT',
-  access_token_secret: 'yZVQ5afcdu1LhoT6ATtmOTzNwGKchTCzDppDQZaOMLEUk'
+  consumer_key: '',
+  consumer_secret: '',
+  access_token: '',
+  access_token_secret: ''
 });
 
 function colorize(text) {
